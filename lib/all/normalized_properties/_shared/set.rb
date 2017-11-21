16775de9bff51c27432ac20d775bf63d64aa1f8a
Christopher Aue <mail@christopheraue.net>
2017-11-21 22:55:18 +0100

    refactored Set#prepare_filter

--- lib/all/normalized_properties/_shared/set.rb
+++ lib/all/normalized_properties/_shared/set.rb
@@ -24,13 +24,34 @@ module NormalizedProperties
 
     private def prepare_filter(filter, model = @model)
       filter.inject({}) do |prepared, (prop_name, prop_filter)|
-        model.property_config(prop_name).filter_mapper.call(prop_filter).each do |prop_name2, prop_filter2|
-          prop_model = model.property_config(prop_name2).model
-          if prop_model and prop_filter2.is_a? Hash
-            merge_filter! prepared, model.property_config(prop_name2).filter_mapper.call(prepare_filter(prop_filter2, prop_model))
-          else
-            prepared[prop_name2] = prop_filter2
+        unless prop_config = model.property_config(prop_name)
+          raise ArgumentError, "filter contains unknown property #{model}##{prop_name}"
+        end
+        real_filter = prop_config.filter_mapper.call prop_filter
+
+        real_filter.each do |real_prop_name, real_prop_filter|
+          unless real_prop_config = model.property_config(real_prop_name)
+            raise ArgumentError, "filter contains unknown property #{model}##{real_prop_name}"
           end
+
+          real_prop_filter = if real_prop_config.model
+                               if real_prop_filter.is_a? real_prop_config.model
+                                 real_prop_filter = {id: real_prop_filter.property(:id).value}
+                               end
+
+                               case real_prop_filter
+                               when Hash
+                                 prepare_filter real_prop_filter, real_prop_config.model
+                               when true, false
+                                 real_prop_filter
+                               else
+                                 raise ArgumentError, "filter for property #{model}##{real_prop_name} no hash or boolean"
+                               end
+                             else
+                               real_prop_filter
+                             end
+
+          merge_filter! prepared, {real_prop_name => real_prop_filter}
         end
         prepared
       end
