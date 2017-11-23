ce48af6163db409025a5eaeab63347deede84fe1
Christopher Aue <mail@christopheraue.net>
2017-11-23 22:24:37 +0100

    tested Manual::Attribute#where with filtering by a scalar attribute

--- lib/all/normalized_properties/manual/attribute.rb
+++ lib/all/normalized_properties/manual/attribute.rb
@@ -4,7 +4,15 @@ module NormalizedProperties
       EVENTS_TRIGGERED_BY_WATCHER = false
 
       def value
-        @owner.__send__ @config.name
+        value = @owner.__send__ @config.name
+
+        if @filter and @config.model
+          value if value and @filter.all? do |prop_name, prop_filter|
+                               value.property(prop_name).satisfies? prop_filter
+                             end
+        else
+          value
+        end
       end
     end
   end
--- test/CRuby/normalized_properties/manual/attribute_spec.rb
+++ test/CRuby/normalized_properties/manual/attribute_spec.rb
@@ -1,19 +1,20 @@
 describe NormalizedProperties::Manual::Attribute do
   before do
-    stub_const('AttributeOwner', Class.new do
+    stub_const('PropertyOwner', Class.new do
       extend NormalizedProperties
-
-      def initialize(attribute_value)
-        @attribute = attribute_value
-      end
 
       attr_accessor :attribute
       normalized_attribute :attribute, type: 'Manual'
+
+      attr_accessor :association
+      normalized_attribute :association, type: 'Manual', model: 'PropertyOwner'
     end)
   end
 
   subject(:attribute){ owner.property :attribute }
-  let(:owner){ AttributeOwner.new 'attribute_value' }
+  let(:owner){ PropertyOwner.new }
+
+  before{ owner.attribute = 'attribute_value' }
 
   it{ is_expected.to have_attributes(owner: owner) }
   it{ is_expected.to have_attributes(name: :attribute) }
@@ -32,5 +33,56 @@ describe NormalizedProperties::Manual::Attribute do
     before{ expect(callback).to receive(:call) }
     it{ is_expected.not_to raise_error }
     after{ expect(attribute.value).to eq 'changed_value' }
+  end
+
+  describe "#where" do
+    subject{ association_property.where filter }
+    let(:association_property){ owner.property :association }
+
+    let(:association) do
+      PropertyOwner.new.tap do |association|
+        association.attribute = 'association_value'
+      end
+    end
+    before{ owner.association = association }
+
+    context "when the filter is no hash" do
+      let(:filter){ :no_hash }
+      it{ is_expected.to raise_error ArgumentError, 'filter no hash' }
+    end
+
+    context "when the filter is a hash" do
+      context "when the filter is empty" do
+        let(:filter){ {} }
+        it{ is_expected.to have_attributes(value: association) }
+      end
+
+      context "when filtering by an unknown property" do
+        let(:filter){ {unknown: 'value'} }
+        it{ is_expected.to raise_error ArgumentError, "filter contains unknown property PropertyOwner#unknown" }
+      end
+
+      context "when filtering by an attribute property" do
+        context "when the property does not satisfy the filter" do
+          let(:filter){ {attribute: 'another_value'} }
+          it{ is_expected.to have_attributes(value: nil) }
+
+          context "when the association does not exist" do
+            let(:association){ nil }
+            it{ is_expected.to have_attributes(value: nil) }
+          end
+        end
+
+        context "when the property satisfies the filter" do
+          let(:filter){ {attribute: 'association_value'} }
+          it{ is_expected.to have_attributes(value: association) }
+
+          context "when the association does not exist" do
+            let(:association){ nil }
+            it{ is_expected.to have_attributes(value: nil) }
+          end
+        end
+      end
+    end
   end
 end
