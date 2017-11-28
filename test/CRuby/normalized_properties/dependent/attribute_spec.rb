93c21e272e6ea60eea720a1a87b33d5341336647
Christopher Aue <mail@christopheraue.net>
2017-11-28 18:19:06 +0100

    tested dependent attributes with a model instance as value

--- test/CRuby/normalized_properties/dependent/attribute_spec.rb
+++ test/CRuby/normalized_properties/dependent/attribute_spec.rb
@@ -1,98 +1,300 @@
 describe NormalizedProperties::Dependent::Attribute do
   before do
-    stub_const('AttributeOwner', Class.new do
-      extend NormalizedProperties
-
-      def attribute
-        @value ||= 'attribute_value'
-      end
-      normalized_attribute :attribute, type: 'Manual'
-
-      def attribute=(new_value)
-        @value = new_value
-        property(:attribute).changed!
-      end
-
-      def child
-        @child ||= self.class.new
-      end
-      normalized_attribute :child, type: 'Manual'
-
-      normalized_attribute :symbol_dependent, type: 'Dependent',
-        sources: :attribute,
-        value: ->(sources){ "dependent_#{sources[:attribute].value}" },
-        filter: ->(filter){ {attribute: filter.sub("dependent_", "")} }
-
-      normalized_attribute :array_dependent, type: 'Dependent',
-        sources: [:attribute],
-        value: ->(sources){ "dependent_#{sources[:attribute].value}" },
-        filter: ->(filter){ {attribute: filter.sub("dependent_", "")} }
-
-      normalized_attribute :hash_dependent, type: 'Dependent',
-        sources: {child: :attribute},
-        value: ->(sources){ "dependent_#{sources[:child][:attribute].value}" },
-        filter: ->(filter){ {child: {attribute: filter.sub("dependent_", "")}} }
-
-      normalized_attribute :mixed_dependent, type: 'Dependent',
-        sources: {child: [child: :attribute]},
-        value: ->(sources){ "dependent_#{sources[:child][:child][:attribute].value}" },
-        filter: ->(filter){ {child: {attribute: filter.sub("dependent_", "")}} }
-    end)
+    stub_const('AttributeOwner', Class.new{ extend NormalizedProperties })
   end
-  
-  let(:dependent_owner){ AttributeOwner.new }
 
-  shared_examples "for an attribute property" do|property_name|
-    subject(:dependent_attribute){ dependent_owner.property property_name }
+  let(:owner){ AttributeOwner.new }
 
-    it{ is_expected.to have_attributes(owner: dependent_owner) }
-    it{ is_expected.to have_attributes(name: property_name) }
-    it{ is_expected.to have_attributes(to_s: "#{dependent_owner}##{property_name}") }
-    it{ is_expected.to have_attributes(value: 'dependent_attribute_value') }
+  describe "an attribute with a simple value" do
+    before do
+      class AttributeOwner
+        def attribute
+          @value ||= 'attribute_value'
+        end
+        normalized_attribute :attribute, type: 'Manual'
 
-    describe "watching a change" do
-      subject{ attribute_owner.attribute = 'changed_value' }
+        def attribute=(new_value)
+          @value = new_value
+          property(:attribute).changed!
+        end
 
-      before{ dependent_attribute.on(:changed){ |*args| callback.call *args } }
-      let(:callback){ proc{} }
+        def child
+          @child ||= self.class.new
+        end
+        normalized_attribute :child, type: 'Manual'
 
-      before{ expect(callback).to receive(:call) }
-      it{ is_expected.not_to raise_error }
-      after{ expect(dependent_attribute.value).to eq 'dependent_changed_value' }
-    end
+        normalized_attribute :symbol_dependent, type: 'Dependent',
+          sources: :attribute,
+          value: ->(sources){ "dependent_#{sources[:attribute].value}" },
+          filter: ->(filter){ {attribute: filter.sub("dependent_", "")} }
 
-    describe "#satisfies?" do
-      subject{ dependent_attribute.satisfies? filter }
+        normalized_attribute :array_dependent, type: 'Dependent',
+          sources: [:attribute],
+          value: ->(sources){ "dependent_#{sources[:attribute].value}" },
+          filter: ->(filter){ {attribute: filter.sub("dependent_", "")} }
 
-      context "when the attribute does not satisfy the filter" do
-        let(:filter){ "another_value" }
-        it{ is_expected.to be false }
-      end
+        normalized_attribute :hash_dependent, type: 'Dependent',
+          sources: {child: :attribute},
+          value: ->(sources){ "dependent_#{sources[:child][:attribute].value}" },
+          filter: ->(filter){ {child: {attribute: filter.sub("dependent_", "")}} }
 
-      context "when the attribute satisfies the filter" do
-        let(:filter){ "dependent_attribute_value" }
-        it{ is_expected.to be true }
+        normalized_attribute :mixed_dependent, type: 'Dependent',
+          sources: {child: [child: :attribute]},
+          value: ->(sources){ "dependent_#{sources[:child][:child][:attribute].value}" },
+          filter: ->(filter){ {child: {attribute: filter.sub("dependent_", "")}} }
       end
     end
+
+    shared_examples "for an attribute property" do|property_name|
+      subject(:dependent_attribute){ owner.property property_name }
+
+      it{ is_expected.to have_attributes(owner: owner) }
+      it{ is_expected.to have_attributes(name: property_name) }
+      it{ is_expected.to have_attributes(to_s: "#{owner}##{property_name}") }
+      it{ is_expected.to have_attributes(value: 'dependent_attribute_value') }
+
+      describe "watching a change" do
+        subject{ attribute_owner.attribute = 'changed_value' }
+
+        before{ dependent_attribute.on(:changed){ |*args| callback.call *args } }
+        let(:callback){ proc{} }
+
+        before{ expect(callback).to receive(:call) }
+        it{ is_expected.not_to raise_error }
+        after{ expect(dependent_attribute.value).to eq 'dependent_changed_value' }
+      end
+
+      describe "#satisfies?" do
+        subject{ dependent_attribute.satisfies? filter }
+
+        context "when the attribute does not satisfy the filter" do
+          let(:filter){ "another_value" }
+          it{ is_expected.to be false }
+        end
+
+        context "when the attribute satisfies the filter" do
+          let(:filter){ "dependent_attribute_value" }
+          it{ is_expected.to be true }
+        end
+      end
+    end
+
+    context "when the attribute has a symbol source" do
+      let(:attribute_owner){ owner }
+      include_examples "for an attribute property", :symbol_dependent
+    end
+
+    context "when the attribute has an array source" do
+      let(:attribute_owner){ owner }
+      include_examples "for an attribute property", :array_dependent
+    end
+
+    context "when the attribute has a hash source" do
+      let(:attribute_owner){ owner.child }
+      include_examples "for an attribute property", :hash_dependent
+    end
+
+    context "when the attribute has a mixed source" do
+      let(:attribute_owner){ owner.child.child }
+      include_examples "for an attribute property", :mixed_dependent
+    end
   end
 
-  context "when the attribute has a symbol source" do
-    let(:attribute_owner){ dependent_owner }
-    include_examples "for an attribute property", :symbol_dependent
-  end
+  describe "an attribute with a model instance as value" do
+    before do
+      stub_const('ManualObject', Class.new do
+        extend NormalizedProperties
 
-  context "when the attribute has an array source" do
-    let(:attribute_owner){ dependent_owner }
-    include_examples "for an attribute property", :array_dependent
-  end
+        def initialize(value)
+          @value = value
+        end
 
-  context "when the attribute has a hash source" do
-    let(:attribute_owner){ dependent_owner.child }
-    include_examples "for an attribute property", :hash_dependent
-  end
+        alias id __id__
+        normalized_attribute :id, type: 'Manual'
 
-  context "when the attribute has a mixed source" do
-    let(:attribute_owner){ dependent_owner.child.child }
-    include_examples "for an attribute property", :mixed_dependent
+        attr_reader :value
+        normalized_attribute :value, type: 'Manual'
+      end)
+
+      stub_const('DependentObject', Class.new do
+        extend NormalizedProperties
+
+        def initialize(object)
+          @object = object
+        end
+
+        attr_reader :object
+        normalized_attribute :object, type: 'Manual'
+
+        def ==(other)
+          @object == other&.object
+        end
+        alias eql? ==
+
+        def hash
+          @object.hash
+        end
+      end)
+    end
+
+    describe "an object dependent attribute" do
+      subject(:dependent_attribute){ owner.property :object_dependent }
+
+      before do
+        class AttributeOwner
+          attr_accessor :object
+          normalized_attribute :object, type: 'Manual', item_model: 'ManualObject'
+
+          normalized_attribute :object_dependent, type: 'Dependent', value_model: 'DependentObject',
+            sources: :object,
+            value: ->(sources){ DependentObject.new sources[:object].value },
+            filter: ->(filter){ {object: (filter.is_a? DependentObject) ? filter.object : filter} }
+        end
+      end
+
+      let(:object) { ManualObject.new 'object' }
+      before{ owner.object = object }
+
+      it{ is_expected.to have_attributes(owner: owner) }
+      it{ is_expected.to have_attributes(name: :object_dependent) }
+      it{ is_expected.to have_attributes(to_s: "#{owner}#object_dependent") }
+      it{ is_expected.to have_attributes(value: DependentObject.new(object)) }
+
+      describe "watching a change" do
+        subject do
+          owner.object = changed_object
+          owner.property(:object).changed!
+        end
+
+        let(:changed_object){ ManualObject.new 'changed_object' }
+
+        before{ dependent_attribute.on(:changed){ |*args| callback.call *args } }
+        let(:callback){ proc{} }
+
+        before{ expect(callback).to receive(:call) }
+        it{ is_expected.not_to raise_error }
+        after{ expect(dependent_attribute.value).to eq DependentObject.new(changed_object) }
+      end
+
+      describe "#satisfies?" do
+        subject{ dependent_attribute.satisfies? filter }
+
+        context "when the filter is a hash with property sub filters" do
+          context "when the set does not satisfy the filter" do
+            let(:filter){ {value: "another_object"} }
+            it{ is_expected.to be false }
+          end
+
+          context "when the set satisfies the filter" do
+            let(:filter){ {value: 'object'} }
+            it{ is_expected.to be true }
+          end
+        end
+
+        context "when the filter is an item directly" do
+          context "when the set does not satisfy the filter" do
+            let(:filter){ DependentObject.new another_object }
+            let(:another_object) { ManualObject.new 'another_object' }
+            it{ is_expected.to be false }
+          end
+
+          context "when the set satisfies the filter" do
+            let(:filter){ DependentObject.new object }
+            it{ is_expected.to be true }
+          end
+        end
+      end
+    end
+
+    describe "a set dependent attribute" do
+      subject(:dependent_attribute){ owner.property :set_dependent }
+
+      before do
+        class AttributeOwner
+          def initialize
+            @set = []
+          end
+
+          attr_accessor :set
+          normalized_set :set, type: 'Manual', item_model: 'ManualObject'
+
+          normalized_attribute :set_dependent, type: 'Dependent', value_model: 'DependentObject',
+            sources: :set,
+            value: ->(sources){ DependentObject.new sources[:set].value.find{ |item| item.value == 'item2' } },
+            filter: ->(filter){ {set: (filter.is_a? DependentObject) ? filter.object : filter} }
+        end
+      end
+
+      let(:item1){ ManualObject.new 'item1' }
+      let(:item2){ ManualObject.new 'item2' }
+      let(:item3){ ManualObject.new 'item3' }
+      before{ owner.set = [item1, item2, item3] }
+
+      it{ is_expected.to have_attributes(owner: owner) }
+      it{ is_expected.to have_attributes(name: :set_dependent) }
+      it{ is_expected.to have_attributes(to_s: "#{owner}#set_dependent") }
+      it{ is_expected.to have_attributes(value: DependentObject.new(item2)) }
+
+      describe "watching a change" do
+        def remove
+          owner.set.delete item2
+          owner.property(:set).removed! item2
+        end
+
+        def add
+          owner.set.push item2
+          owner.property(:set).added! item2
+        end
+
+        context "when the base item is removed from the set" do
+          subject{ remove }
+          before{ dependent_attribute.on(:changed){ |*args| callback.call *args } }
+          let(:callback){ proc{} }
+
+          before{ expect(callback).to receive(:call) }
+          it{ is_expected.not_to raise_error }
+          after{ expect(dependent_attribute.value).to eq nil }
+        end
+
+        context "when the base item is added the set" do
+          before{ remove }
+          subject{ add }
+          before{ dependent_attribute.on(:changed){ |*args| callback.call *args } }
+          let(:callback){ proc{} }
+
+          before{ expect(callback).to receive(:call) }
+          it{ is_expected.not_to raise_error }
+          after{ expect(dependent_attribute.value).to eq DependentObject.new(item2) }
+        end
+      end
+
+      describe "#satisfies?" do
+        subject{ dependent_attribute.satisfies? filter }
+
+        context "when the filter is a hash with property sub filters" do
+          context "when the set does not satisfy the filter" do
+            let(:filter){ {value: "item1"} }
+            it{ is_expected.to be false }
+          end
+
+          context "when the set satisfies the filter" do
+            let(:filter){ {value: 'item2'} }
+            it{ is_expected.to be true }
+          end
+        end
+
+        context "when the filter is an item directly" do
+          context "when the set does not satisfy the filter" do
+            let(:filter){ DependentObject.new(item1) }
+            it{ is_expected.to be false }
+          end
+
+          context "when the set satisfies the filter" do
+            let(:filter){ DependentObject.new(item2) }
+            it{ is_expected.to be true }
+          end
+        end
+      end
+    end
   end
 end
