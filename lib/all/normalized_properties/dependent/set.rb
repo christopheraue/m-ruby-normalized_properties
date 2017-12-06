bfc9259d8c65c7ef25ddd60e2c45cfdd8d501875
Christopher Aue <mail@christopheraue.net>
2017-12-06 17:49:48 +0100

    refactored #satisfies? of dependent properties

--- lib/all/normalized_properties/dependent/set.rb
+++ lib/all/normalized_properties/dependent/set.rb
@@ -12,16 +12,8 @@ module NormalizedProperties
       end
 
       def satisfies?(filter)
-        filter = case filter
-                 when Hash
-                   Filter.new(:and, filter)
-                 when Instance
-                   Filter.new(:and, filter.to_filter)
-                 else
-                   filter
-                 end
-
-        filter = filter.and @config.value_filter.call(value) if @config.value_filter
+        filter = filter.to_filter if item_model and filter.is_a? item_model
+        filter = Filter.new :and, filter, @config.value_filter.call(value) if @config.value_filter
         filter = Filter.new :and, @config.sources_filter.call(filter)
         owner.satisfies? filter
       end
